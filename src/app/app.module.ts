4a956f67dca29b08012ba675f5dba42c3ac362be
gokel166 <okellogeorge40@yahoo.com>
2017-12-26 23:33:38 -0600

    Add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ChatFormComponent } from './chat-form/chat-form.component';
 import { ChatroomComponent } from './chatroom/chatroom.component';
+import { FeedComponent } from './feed/feed.component';
+import { MessageComponent } from './message/message.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatFormComponent,
-    ChatroomComponent
+    ChatroomComponent,
+    FeedComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule

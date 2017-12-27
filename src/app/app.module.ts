5a091630bceb996f5bae6b071c5d4c83cd00e695
gokel166 <okellogeorge40@yahoo.com>
2017-12-26 23:21:36 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ChatFormComponent } from './chat-form/chat-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatFormComponent
   ],
   imports: [
     BrowserModule

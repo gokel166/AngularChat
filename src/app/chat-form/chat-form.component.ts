23479e413a9fec32e6bbeab32d1c499dc724c588
gokel166 <okellogeorge40@yahoo.com>
2017-12-31 00:17:18 -0600

    Form Authentication layers

--- src/app/chat-form/chat-form.component.ts
+++ src/app/chat-form/chat-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../services/chat.service';
 
 @Component({
   selector: 'app-chat-form',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private chat: ChatService) { }
 
   ngOnInit() {
+  }
+
+  send() {
+    this.chat.sendMessage(this.message);
   }
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,12 @@
+
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
+
+export class AuthService {
+    private user: Observable<firebase.User>;
+    private authState: any;
+}
